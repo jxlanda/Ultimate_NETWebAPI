860c79c525847eecf7f5209af9b4d94d15c31fd5
José Luis Cuevas Landa <cuevas.joseluis@outlook.com>
2022-04-10 01:50:24 -0700

    In Progress AuthContrller

---
+++ Entities/DataTransferObjects/Authorization/RefreshTokenDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects.Authorization
+{
+    public class RefreshTokenDTO
+    {
+        public string RefreshToken { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
--- NET.WebAPI/Controllers/AuthController.cs
+++ NET.WebAPI/Controllers/AuthController.cs
@@ -1,8 +1,15 @@
-﻿using Entities.Models;
+﻿using Contracts;
+using Entities.DataTransferObjects.Authorization;
+using Entities.Models;
+using Entities.Models.Database;
+using Entities.Types;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using NET.WebAPI.Extensions;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -17,40 +24,138 @@ namespace NET5.WebAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private IConfiguration _config;
+        private readonly IRepositoryWrapper _repository;
+        private readonly EncryptionService _encryption;
+        private readonly JWTService _jwtService;
 
-        public AuthController(IConfiguration config)
+        public AuthController(EncryptionService encryption, JWTService jwtService,
+            IRepositoryWrapper repository)
         {
-            _config = config;
+            _repository = repository;
+            _encryption = encryption;
+            _jwtService = jwtService;
         }
 
-        // GET api/values
-        [HttpPost, Route("login")]
-        public IActionResult Login([FromBody] LoginModel user)
+        [HttpPost, Route("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDTO user)
         {
+            string encryptedPassword = _encryption.EncryptString(user.Password);
+            IQueryable<User> query = await _repository.User.GetAllByConditionAsync(x =>
+                x.Email == user.Email.ToLowerInvariant() &&
+                x.Password == encryptedPassword);
+
+            User userFound = query.FirstOrDefault();
+
+            if (userFound == null) return NotFound();
+
+            SigningCredentials signingCredentials = _jwtService.GetSigningCredentials(JWTType.Token);
+            List<Claim> claims = _jwtService.GetClaims(userFound);
+            // JWT
+            JwtSecurityToken tokenOptions = _jwtService.GenerateTokenOptions(signingCredentials, claims, JWTType.Token);
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            // Refresh Token
+            SigningCredentials refreshSigningCredentials = _jwtService.GetSigningCredentials(JWTType.RefreshToken);
+            JwtSecurityToken refreshTokenOptions = _jwtService.GenerateTokenOptions(refreshSigningCredentials, claims, JWTType.RefreshToken);
+            string refreshToken = new JwtSecurityTokenHandler().WriteToken(refreshTokenOptions);
+
+            return Ok(new { token, refreshToken });
+        }
+
+        [HttpPost("ForgotPassword")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDTO forgotPasswordDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            IQueryable<User> query = await _repository.User.GetAllByConditionAsync(x => x.Email == forgotPasswordDTO.Email);
+            User user = query.FirstOrDefault();
 
             if (user == null)
+                return BadRequest("Invalid Request");
+
+            SigningCredentials signingCredentials = _jwtService.GetSigningCredentials(JWTType.ResetPassword);
+            List<Claim> claims = _jwtService.GetClaims(user);
+            JwtSecurityToken tokenOptions = _jwtService.GenerateTokenOptions(signingCredentials, claims, JWTType.ResetPassword);
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            Dictionary<string, string> param = new()
             {
-                return BadRequest("Invalid client request");
-            }
-            if (user.UserName == "admin" && user.Password == "root")
-            {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: _config["Jwt:Issuer"],
-                    audience: _config["Jwt:Issuer"],
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signinCredentials
-                );
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                return Ok(new { Token = tokenString });
-            }
-            else
-            {
-                return Unauthorized();
-            }
+                { "token", token }
+            };
+
+            string callback = QueryHelpers.AddQueryString(forgotPasswordDTO.ClientURI, param);
+
+            // Send Email Notification
+            // Update ResetToken DB
+
+            return Ok();
+        }
+
+        [HttpPost("ResetPassword")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO resetPasswordDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            IQueryable<User> query = await _repository.User.GetAllByConditionAsync(x => x.Email == resetPasswordDTO.Email);
+            User user = query.FirstOrDefault();
+
+            if (user == null)
+                return BadRequest("Invalid Request");
+
+            bool isValidToken = _jwtService.IsTokenValid(resetPasswordDTO.Token, JWTType.ResetPassword);
+            if (!isValidToken) return Unauthorized();
+
+            // Validate Token DB
+            
+            return Ok();
+        }
+
+        [HttpPost("Token")]
+        public async Task<IActionResult> Token([FromBody] RefreshTokenDTO tokenDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            bool isValidToken = _jwtService.IsTokenValid(tokenDTO.RefreshToken, JWTType.RefreshToken);
+            if (!isValidToken) return Unauthorized();
+
+            IQueryable<User> query = await _repository.User.GetAllByConditionAsync(x =>
+                x.Email == tokenDTO.Email.ToLowerInvariant());
+            User userFound = query.FirstOrDefault();
+
+            if (userFound == null)
+                return NotFound();
+
+            // JWT
+            SigningCredentials signingCredentials = _jwtService.GetSigningCredentials(JWTType.Token);
+            List<Claim> claims = _jwtService.GetClaims(userFound);
+            JwtSecurityToken tokenOptions = _jwtService.GenerateTokenOptions(signingCredentials, claims, JWTType.Token);
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            // Refresh Token
+            SigningCredentials refreshSigningCredentials = _jwtService.GetSigningCredentials(JWTType.RefreshToken);
+            JwtSecurityToken refreshTokenOptions = _jwtService.GenerateTokenOptions(refreshSigningCredentials, claims, JWTType.RefreshToken);
+            string refreshToken = new JwtSecurityTokenHandler().WriteToken(refreshTokenOptions);
+
+            return Ok(new { token, refreshToken });
+        }
+
+        [HttpPost]
+        [Route("RevokeToken/{refreshToken}")]
+        public async Task<IActionResult> RevokeRefreshToken(string refreshToken)
+        {
+            if (refreshToken == null) return NotFound();
+            Claim emailClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+            if (emailClaim == null) return BadRequest();
+
+            IQueryable<User> query = await _repository.User.GetAllByConditionAsync(x =>
+                x.Email == emailClaim.Value.ToLowerInvariant());
+            User userFound = query.AsNoTracking().FirstOrDefault();
+            if (userFound == null) return NotFound();
+
+            // Revoke token in DB
+
+            return Ok();
         }
     }
 }
